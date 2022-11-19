7311384e834d1c326975013e7c721ef5b441d666
alikaratas17 <alikaratas17@ku.edu.tr>
2022-11-19 16:51:48 +0300

    Able to send different mixture options to the adafruit io server

--- App/mixture_selector/lib/main.dart
+++ App/mixture_selector/lib/main.dart
@@ -11,23 +11,36 @@ class MyApp extends StatelessWidget{
     return MaterialApp(title: 'Select a Mixture',
     home:Scaffold(appBar: AppBar(title: Text('Why is this title different from prev?')),body: Column(children: [
       Text('Please Select one of below'),
-      ElevatedButton(onPressed: func1, child: Text('Option 1')),
-      ElevatedButton(onPressed: null, child: Text('Option 2')),
-      ElevatedButton(onPressed: null, child: Text('Option 3')),
-      ElevatedButton(onPressed: null, child: Text('Option 4')),
+      ElevatedButton(onPressed: opt1_func, child: Text('Option 1')),
+      ElevatedButton(onPressed: opt2_func, child: Text('Option 2')),
+      ElevatedButton(onPressed: opt3_func, child: Text('Option 3')),
+      ElevatedButton(onPressed: opt4_func, child: Text('Option 4')),
     ]),));
   }
 
 }
-void func1() async{
+void opt1_func() async{
   String my_url = 'https://io.adafruit.com/api/v2/akaratas17/feeds/message/data?X-AIO-Key='+ my_key.key;
-  String my_json=jsonEncode( {'json':{'datum':{'value':'myMixture'}}});
-  print(my_url);
-  print(my_json);
-  final response = await http.post(Uri.parse(my_url),body:my_json,);
-  if (response.statusCode==200){
-    print("YES");
-  }
+  String option = "Option 1";
+  final response = await http.post(Uri.parse(my_url),body:{'value':option});
+  print(response.statusCode.toString());
+}
+void opt2_func() async{
+  String my_url = 'https://io.adafruit.com/api/v2/akaratas17/feeds/message/data?X-AIO-Key='+ my_key.key;
+  String option = "Option 2";
+  final response = await http.post(Uri.parse(my_url),body:{'value':option});
+  print(response.statusCode.toString());
+}
+void opt3_func() async{
+  String my_url = 'https://io.adafruit.com/api/v2/akaratas17/feeds/message/data?X-AIO-Key='+ my_key.key;
+  String option = "Option 3";
+  final response = await http.post(Uri.parse(my_url),body:{'value':option});
+  print(response.statusCode.toString());
+}
+void opt4_func() async{
+  String my_url = 'https://io.adafruit.com/api/v2/akaratas17/feeds/message/data?X-AIO-Key='+ my_key.key;
+  String option = "Option 4";
+  final response = await http.post(Uri.parse(my_url),body:{'value':option});
   print(response.statusCode.toString());
 }
 /*
