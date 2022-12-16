20e26d680cda090e98de1bcd4b626ce786eb3091
alikaratas17 <alikaratas17@ku.edu.tr>
2022-12-16 20:31:15 +0300

    Added code to parse for huzzah

--- Arduino_Codes/Final Codes/controller.ino
+++ Arduino_Codes/Final Codes/controller.ino
@@ -17,8 +17,6 @@ AdafruitIO_Feed *message = io.feed("message");
 
 void setup(){
   setup_pins();
-  Serial.begin(9600); 
-  Serial.println("Starting!!");
   state = WAITING_FOR_MESSAGE_STATE;
   Serial.begin(115200);
   while(!Serial);
@@ -41,6 +39,29 @@ void setup_pins(){
 
 void handleMessage(AdafruitIO_Data *data){
   Serial.println(data->toString());
+  String result = data->toString();
+  int inputs [4];
+  int i = 0;
+  int current = 0;
+  int prev = 0;
+  while(i !=3){
+    if (result[current]==','){
+      String substr = result.substring(prev,current);
+      //Serial.println(substr);
+      //Serial.println(prev);
+      //Serial.println(current);
+    inputs[i] = substr.toInt();
+    prev = current+1;
+    i++;
+    }
+      current ++;
+  }
+  String substr = result.substring(current,result.length());
+  inputs[3] = substr.toInt();
+  Serial.println("__________");
+  Serial.println(inputs[0]);
+  Serial.println(inputs[1]);
+  Serial.println(inputs[2]);
+  Serial.println(inputs[3]);
   delay(500);
-
 }
